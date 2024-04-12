0474cf50899baca9baef08b668f0ffc6343907f4
jonaaldas <jonaaldas@gmail.com>
2024-04-11 21:32:24 -0400

    added new files

---
+++ app.js
@@ -0,0 +1,18 @@
+import {CronJob} from 'cron';
+import {createClient} from '@supabase/supabase-js';
+import 'dotenv/config';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+new CronJob(
+	'* * * * *',
+	async function() {
+		const {data, error} = await supabase.from('restaurants').select('*');
+		console.log('🚀 ~ allRestaurants:', data);
+	},
+	null,
+	true,
+	'America/Los_Angeles'
+);
