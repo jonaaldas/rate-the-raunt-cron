40eaa8aa13bdb67e3f510781436881a9100c32b5
jonaaldas <jonaaldas@gmail.com>
2024-04-11 21:51:08 -0400

    deleted

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-import {CronJob} from 'cron';
 import {createClient} from '@supabase/supabase-js';
 import 'dotenv/config';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-new CronJob(
-	'0 0 */3 * *',
-	async function() {
-		const {data, error} = await supabase.from('restaurants').select('*');
-		console.log('🚀 ~ allRestaurants:', data);
-	},
-	null,
-	true,
-	'America/Los_Angeles'
-);
+const {data, error} = await supabase.from('restaurants').select('*');
+console.log('🚀 ~ allRestaurants:', data);
